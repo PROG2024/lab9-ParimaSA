691020c9441565cea1f9344e498ec523003ab88d
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-03-12 20:18:54 +0700

    Problem 1 and 2

--- circle.py
+++ circle.py
@@ -9,7 +9,14 @@ class Circle:
         
         :param radius: radius of the circle, may be zero.
         :raises ValueError: if radius is negative.
+
+        example
+        >>> Circle(-5)
+        Traceback (most recent call last):
+        ...
+        ValueError: radius must be non-negative
         """
+
         if radius < 0:
             raise ValueError("radius must be non-negative")
         self.radius = radius
@@ -19,6 +26,13 @@ class Circle:
         area of this circle and another circle.
         Since area is pi*r**2, the radii of the 3 circles
         should form a Pythagorean triple (r1^2 + r2^2 = r3^2)
+
+        example
+        >>> c1 = Circle(3)
+        >>> c2 = Circle(4)
+        >>> c3 = c1.add_area(c2)
+        >>> c3.get_radius()
+        5.0
         """
         r1 = self.get_radius()
         r2 = circle.get_radius()
--- test_circle.py
+++ test_circle.py
@@ -7,6 +7,39 @@ Write unit tests as described in README.md.
 3. Unit test that circle constructor raises exception of radius is negative.
 
 """
+import math
+import unittest
+
 from circle import Circle
 
-# TODO write 3 tests as described above
+
+class CircleTest(unittest.TestCase):
+    def test_add_area_with_two_positive_circle(self):
+        """
+        The area of new circle should equal to the combined area of circle1 and circle2
+        and the radius should equal to sqrt(r1^2 + r2^2) from the pythagorean laws(r1^2 + r2^2 = r3^2).
+        """
+        circle1 = Circle(5)
+        circle2 = Circle(10)
+        new_circle = circle1.add_area(circle2)
+        radius = math.hypot(5, 10)
+        area = circle1.get_area() + circle2.get_area()
+        self.assertEqual(radius, new_circle.get_radius())
+        self.assertEqual(area, new_circle.get_area())
+
+    def test_add_area_with_one_zero_circle(self):
+        """
+        When one of the circle has radius 0, add area with non-zero circle,
+        new circle should have the same radius and area with non-zero circle.
+        """
+        circle1 = Circle(5)
+        circle2 = Circle(0)
+        new_circle = circle1.add_area(circle2)
+        area = circle1.get_area()
+        self.assertEqual(5, new_circle.get_radius())
+        self.assertEqual(area, new_circle.get_area())
+
+    def test_circle_with_negative_radius(self):
+        """Circle constructor should raises exception if the radius is negative."""
+        with self.assertRaises(Exception):
+            negative_circle = Circle(-5)
